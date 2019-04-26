c1ef7a8aeb36598f00da75cc047f0185134187b0
Peter Kingori <kpndegwa@gmail.com>
2019-04-26 12:11:00 +0300

    create array to hold the length values of a triangle entered by the user

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Function to use the values entered and determine type of triangle
+function triangletype() {
+  //array to hold the lengths of the triangle sides inputted by user
+  var lengths = [];
+  length[0] = document.getElementById("value1").value;
+  length[1] = document.getElementById("value2").value;
+  length[2] = document.getElementById("value3").value;
+  
+}
