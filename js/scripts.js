7249023f1501ca82c049e91597ed9c7e455a3414
Peter Kingori <kpndegwa@gmail.com>
2019-04-26 15:20:40 +0300

    correct the conditions in JS script that determines if values entered form an isosceles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 //Function to use the values entered and determine type of triangle
 function triangletype() {
   //array to hold the lengths of the triangle sides inputted by user
-  var lengths = [];
-  length[0] = document.getElementById("value1").value;
-  length[1] = document.getElementById("value2").value;
-  length[2] = document.getElementById("value3").value;
+  //var lengths = [];
+  var length1 = parseInt(document.getElementById("value1").value);
+  var length2 = parseInt(document.getElementById("value2").value);
+  var length3 = parseInt(document.getElementById("value3").value);
 
-  if (((length[0] + length[1]) > length[2]) || ((length[0] + length[2]) > length[1]) || ((length[1] + length[2]) > length[0])) {
-    if (length[0] === length[1] || length[0] === length[2] || length[1] === length[2]) {
-      print on screen isosceles triangle
+  if (((length1 + length2) > length3) && ((length1 + length3) > length2) && ((length2 + length3) > length1)) {
+    if ((length1 === length2 && length1 !== length3) || (length1 === length3 && length1 !== length2) || (length2 === length3 && length2 !== length1)) {
+      //print on screen isosceles triangle
+      alert("Isosceles Triangle");
+    } else if ((length1 === length2) && (length2 === length3)) {
+      //print on screen equilateral triangle
+      alert("Equilateral Triangle");
+    } else {
+      //print on screen scalene triangle
+      alert("Scalene Triangle");
     }
-    else if (length[0] === length[1] && length[1] === length[2]) {
-      print on screen equilateral triangle
-    }
-    else {
-      print on screen scalene triangle
-    }
+  } else {
+    //print the values cannot form a triangle
+    alert("Those values cannot form a triangle");
   }
-  else {
-    print the values cannot form a triangle
-  }
+
 }
