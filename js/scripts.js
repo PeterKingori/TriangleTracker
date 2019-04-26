e9416990cf1f5c598055adaa583b1707b40f15e3
Peter Kingori <kpndegwa@gmail.com>
2019-04-26 15:54:46 +0300

    change variables that hold values entered to values in the array created earlier; tested the application and it is still working as expected

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //Function to use the values entered and determine type of triangle
 function triangletype() {
   //array to hold the lengths of the triangle sides inputted by user
-  //var lengths = [];
-  var length1 = parseInt(document.getElementById("value1").value);
-  var length2 = parseInt(document.getElementById("value2").value);
-  var length3 = parseInt(document.getElementById("value3").value);
+  var length = [];
+  length[0] = parseInt(document.getElementById("value1").value);
+  length[1] = parseInt(document.getElementById("value2").value);
+  length[2] = parseInt(document.getElementById("value3").value);
 
-  if (((length1 + length2) > length3) && ((length1 + length3) > length2) && ((length2 + length3) > length1)) {
-    if ((length1 === length2 && length1 !== length3) || (length1 === length3 && length1 !== length2) || (length2 === length3 && length2 !== length1)) {
+  if (((length[0] + length[1]) > length[2]) && ((length[0] + length[2]) > length[1]) && ((length[1] + length[2]) > length[0])) {
+    if ((length[0] === length[1] && length[0] !== length[2]) || (length[0] === length[2] && length[0] !== length[1]) || (length[1] === length[2] && length[1] !== length[0])) {
       //print on screen isosceles triangle
       document.getElementById('displayresult').innerHTML = "That is an Isosceles Triangle";
-    } else if ((length1 === length2) && (length2 === length3)) {
+    } else if ((length[0] === length[1]) && (length[1] === length[2])) {
       //print on screen equilateral triangle
       document.getElementById('displayresult').innerHTML = "That is an Equilateral Triangle";
     } else {
